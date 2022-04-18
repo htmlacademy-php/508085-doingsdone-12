0f17fee8e2f6fee4e0cf0d03946e6cddbed87acd
Дмитрий <galigin01@gmail.com>
2022-04-18 12:13:16 +0300

    Финальная версия

--- index.php
+++ index.php
@@ -180,58 +180,8 @@ $tasks = [
                             <td class="task__controls">
                                 </td>
                         </tr>
-
-
-
-
                         <?php endif; ?>
                         <?php endforeach; ?>
-                  
-
-
-
-
-
-
-
-
-                            <!--<tr class="tasks__item task">
-                            <td class="task__select">
-                                <label class="checkbox task__checkbox">
-                                    <input class="checkbox__input visually-hidden task__checkbox" type="checkbox" value="1">
-                                    <span class="checkbox__text"><= $one_task["Task_name"]; ?></span>
-                                </label>
-                            </td>
-
-                            <td class="task__file">
-                                <a class="download-link" href="#"></a>
-                            </td>
-
-                            <td class="task__date"><= $one_task["Deadline"]; ?></td>
-                            <td class="task__controls">
-                                </td>
-                        </tr>
-                        <php endforeach; ?>
-
-
-                        <php if ($show_complete_tasks == 1): ?>
-                            <tr class="tasks__item task task--completed">
-                                <td class="task__select">
-                                    <label class="checkbox task__checkbox">
-                                        <input class="checkbox__input visually-hidden" type="checkbox" checked>
-                                        <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
-                                    </label>
-                                </td>
-
-                                <td class="task__file">
-                                  <a class="download-link" href="#">Home.psd</a>
-                                </td>
-                                <td class="task__date">10.10.2019</td>
-
-                                <td class="task__controls">
-                                </td>
-                            </tr>
-                        <php endif; ?>-->
                         <!--показывать следующий тег <tr/>, если переменная $show_complete_tasks равна единице-->
                     </table>
                 </main>
