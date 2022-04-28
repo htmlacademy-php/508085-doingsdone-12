e2d816c35b7bebcbdcbe2d2c688e179ba9f8d699
Дмитрий <galigin01@gmail.com>
2022-04-28 15:24:23 +0300

    Готово без защиты от xss атак

--- index.php
+++ index.php
@@ -4,62 +4,11 @@ require_once 'helpers.php';
 // показывать или нет выполненные задачи
 $show_complete_tasks = rand(0, 1);
 
-//projects = 
-
-$tasks = [
-    [
-        'task_name' => 'Собеседование в IT компании',
-        'deadline' => '01.12.2019',
-        'project' => 'Работа',
-        'ready' => false
-    ],
-    [
-        'task_name' => 'Выполнить тестовое задание',
-        'deadline' => '25.12.2019',
-        'project' => 'Работа',
-        'ready' => false
-    ],
-    [
-        'task_name' => 'Сделать задание первого раздела',
-        'deadline' => '21.12.2019',
-        'project' => 'Учеба',
-        'ready' => true
-    ],
-    [
-        'task_name' => 'Встреча с другом',
-        'deadline' => '22.12.2019',
-        'project' => 'Входящие',
-        'ready' => false
-    ],
-    [
-        'task_name' => 'Купить корм для кота',
-        'deadline' => null,
-        'project' => 'Домашние дела',
-        'ready' => false
-    ],
-    [
-        'task_name' => 'Заказать пиццу',
-        'deadline' => null,
-        'project' => 'Домашние дела',
-        'ready' => false
-    ],
-
-];
-
-
-
-$name_title = [
-    'user_name' => 'Дмитрий',
-    'title' => 'Дела в порядке'
-];
-
-
-$main = include_template('main.php',  ['projects' => [
-    'Входящие',
-    'Учеба',
-    'Работа',
-    'Домашние дела',
-    'Авто',
+$main = include_template('main.php',  ['projects' => ['Входящие',
+'Учеба',
+'Работа',
+'Домашние дела',
+'Авто',
 ],
 'tasks' => [
     [
@@ -99,9 +48,7 @@ $main = include_template('main.php',  ['projects' => [
         'ready' => false
     ],
 
-],
-] 
-);
+]]);
 
 $layout = include_template('layout.php', 
 ['title' => 'Дела в порядке',
@@ -110,6 +57,5 @@ $layout = include_template('layout.php',
 ]);
 print($layout);
 ?>
-
 
 
