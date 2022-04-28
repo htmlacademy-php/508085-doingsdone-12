0e9bd99d98042500f03c869264dba17714f1c1cf
Дмитрий <galigin01@gmail.com>
2022-04-28 16:40:09 +0300

    Добавили защиту от xss атак

--- templates/main.php
+++ templates/main.php
@@ -5,7 +5,7 @@
                         <ul class="main-navigation__list">
                             <?php foreach ($projects as $one_project): ?>
                             <li class="main-navigation__list-item">
-                                <a class="main-navigation__list-item-link" href="#"><?= $one_project; ?></a>
+                                <a class="main-navigation__list-item-link" href="#"><?= htmlspecialchars($one_project); ?></a>
                                 <span class="main-navigation__list-item-count"><?= count_tasks($tasks, $one_project); ?></span>
                             </li>
                             <?php endforeach; ?>
@@ -69,7 +69,7 @@
                                 <a class="download-link" href="#"></a>
                             </td>
 
-                            <td class="task__date"><?= $one_task["deadline"]; ?></td>
+                            <td class="task__date"><?= htmlspecialchars($one_task["deadline"]); ?></td>
                             <td class="task__controls">
                                 </td>
                         </tr>
