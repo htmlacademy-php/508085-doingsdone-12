cc7e306b795d139febac605edfa97db72db7ccdb
Дмитрий <galigin01@gmail.com>
2022-05-01 18:28:09 +0300

    Сырая функция по пересчету оставшегося кол-ва часов для задачи

--- my_functions.php
+++ my_functions.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * возвращает количество задач относящихся к проекту
+ * Возвращает количество задач относящихся к проекту
  * @param array $tasks массив с задачами
  * @param string $one_project название проекта 
  * @return integer 
@@ -16,3 +16,23 @@ function count_tasks($tasks, $one_project)
      };
      return $count;
 };
+
+
+
+
+/**
+ * Считает количество часов оставшихся до срока истечения задачи
+ * @param $date дата из массива
+ * @return integer 
+ */
+
+
+ function end_time ($date)
+ {
+     $sample_date  = strtotime($date); //2023-07-01'
+     $today = time();
+     $diff_total = $sample_date - $today;
+     $diff_in_hours = ($diff_total / 60)/60;
+
+     return $diff_in_hours;
+ }
