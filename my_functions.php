d0a3b630966496b1de485b874eb5816a382507f5
Дмитрий <galigin01@gmail.com>
2022-05-01 19:51:25 +0300

    Готовое задание, не забыть вернуть даты в массиве в index.php

--- my_functions.php
+++ my_functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Возвращает количество задач относящихся к проекту
  * @param array $tasks массив с задачами
@@ -22,17 +23,22 @@ function count_tasks($tasks, $one_project)
 
 /**
  * Считает количество часов оставшихся до срока истечения задачи
- * @param $date дата из массива
+ * @param $date принимает дату 
  * @return integer 
  */
 
 
- function end_time ($date)
- {
-     $sample_date  = strtotime($date); //2023-07-01'
-     $today = time();
-     $diff_total = $sample_date - $today;
-     $diff_in_hours = ($diff_total / 60)/60;
-
+function count_hours($date)
+{
+     if (is_null($date)) {
+          $diff_in_hours = 100;
+     } elseif ($date) {
+          $sample_date  = strtotime($date);
+          $today = time();
+          $diff_total = $sample_date - $today;
+          $diff_in_hours = ($diff_total / 60) / 60;
+     };
      return $diff_in_hours;
- }
+}
+
+?>
