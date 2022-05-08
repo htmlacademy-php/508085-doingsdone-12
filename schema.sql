cf850a5181964319c99632155f742514203654c8
Дмитрий <galigin01@gmail.com>
2022-05-08 08:36:14 +0300

    sql отформатировали код

--- schema.sql
+++ schema.sql
@@ -1,29 +1,26 @@
-CREATE DATABASE doinngsdone
-DEFAULT CHARACTER SET UTF-8
-DEFAULT COLLATE utf8_genetal_ci
-USE doinngsdone;
+CREATE DATABASE doinngsdone DEFAULT CHARACTER SET UTF -8 DEFAULT COLLATE utf8_genetal_ci USE doinngsdone;
 
 CREATE TABLE users (
     id_user INT AUTO_INCREMENT PRIMARY KEY,
     dt_registered NOT NULL TIMESTAMP;
-    password_user CHAR(64),
-    avatar_path TEXT,
-    project_name CHAR(64),
-    email email VARCHAR(128)
+
+password_user CHAR(64),
+avatar_path TEXT,
+project_name CHAR(64),
+email email VARCHAR(128)
 );
 
 CREATE TABLE projects (
-id_user INT AUTO_INCREMENT PRIMARY KEY,
-project_name NOT NULL CHAR(64),
-user_connect TINYINT DEFAULT 'Yes' 
+    id_user INT AUTO_INCREMENT PRIMARY KEY,
+    project_name NOT NULL CHAR(64),
+    user_connect TINYINT DEFAULT 'Yes'
 );
 
 CREATE TABLE tasks (
-id_user INT AUTO_INCREMENT PRIMARY KEY,
-project_name NOT NULL CHAR(64),
-task_name NOT NULL VARCHAR(128),
-status_ready TINYINT DEFAULT 'No',
-dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-dt_deadline TIMESTAMP
+    id_user INT AUTO_INCREMENT PRIMARY KEY,
+    project_name NOT NULL CHAR(64),
+    task_name NOT NULL VARCHAR(128),
+    status_ready TINYINT DEFAULT 'No',
+    dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    dt_deadline TIMESTAMP
 );
-
