a53b71046fdacde445b2bad0b941d793420ec5ab
Дмитрий <galigin01@gmail.com>
2022-05-09 17:48:44 +0300

    shema.sql создание бд итеррация 4, добавили индексы. поправили базу

--- schema.sql
+++ schema.sql
@@ -5,24 +5,28 @@ CREATE TABLE users (
     dt_registered NOT NULL TIMESTAMP;
     password_hash NOT NULL VARCHAR(128),
     avatar_path VARCHAR(128),
-    project_name NOT NULL CHAR(64),
+    project_name NOT NULL VARCHAR(128),
     email VARCHAR(128)
 );
 
-
 CREATE TABLE projects (
-    user_id INT PRIMARY KEY,
-    id INT AUTO_INCREMENT NOT NULL,
-    project_name NOT NULL CHAR(64),
-    user_connect TINYINT DEFAULT 1
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    project_name NOT NULL VARCHAR(128),
 );
 
 CREATE TABLE tasks (
-    user_id INT PRIMARY KEY,
-    id INT AUTO_INCREMENT NOT NULL ,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
     project_id NOT NULL INT,
     task_name NOT NULL VARCHAR(128),
     status_ready TINYINT DEFAULT 0,
     dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     dt_deadline TIMESTAMP
 );
+
+CREATE INDEX index_user ON users(id, dt_registered, email);
+
+CREATE INDEX index_project ON projects(id, project_name);
+
+CREATE INDEX index_tasks ON projects(id, task_name, dt_add, dt_deadline);
