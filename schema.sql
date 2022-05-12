997dc6466a6b31951c217b8aec2a0d4366b2b481
Дмитрий <galigin01@gmail.com>
2022-05-12 14:34:57 +0300

    финальная корректировка shqm.sql

--- schema.sql
+++ schema.sql
@@ -1,32 +1,30 @@
-CREATE DATABASE doinngsdone DEFAULT CHARACTER SET UTF -8 DEFAULT COLLATE utf8_genetal_ci USE doinngsdone;
+CREATE DATABASE doinngsdone DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+  
+USE doinngsdone;
 
 CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    dt_registered NOT NULL TIMESTAMP;
-    password_hash NOT NULL VARCHAR(128),
-    avatar_path VARCHAR(128),
-    project_name NOT NULL VARCHAR(128),
-    email VARCHAR(128)
+    id INT AUTO_INCREMENT PRIMARY KEY COMMENT 'Идентификатор пользователя',
+    dt_registered TIMESTAMP NOT NULL COMMENT 'Таймстэмп регистрации',
+    password_hash VARCHAR(128) NOT NULL COMMENT 'Хэш пароля',
+    avatar_path VARCHAR(128) COMMENT 'Путь аватарки',
+    email VARCHAR(128) UNIQUE COMMENT 'Емаил адрес'
 );
 
 CREATE TABLE projects (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    project_name NOT NULL VARCHAR(128),
+    id INT AUTO_INCREMENT PRIMARY KEY COMMENT 'Идентификатор проекта',
+    user_id INT NOT NULL COMMENT 'Пользователь, которому принадлежит проект',
+    project_name VARCHAR(128) NOT NULL COMMENT 'Название проекта',
+    INDEX (user_id)
 );
 
 CREATE TABLE tasks (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    project_id NOT NULL INT,
-    task_name NOT NULL VARCHAR(128),
-    status_ready TINYINT DEFAULT 0,
-    dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    dt_deadline TIMESTAMP
+    id INT AUTO_INCREMENT PRIMARY KEY COMMENT 'Идентификатор задачи',
+    user_id INT NOT NULL COMMENT 'Пользователь, которому принадлежит задача',
+    project_id INT NOT NULL COMMENT 'Проект, которому принадлежит задача',
+    task_name VARCHAR(128) NOT NULL COMMENT 'Название задачи',
+    status_ready TINYINT DEFAULT 0 COMMENT 'Статус готовности',
+    dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT 'Таймстэмп заведения задачи',
+    dt_deadline TIMESTAMP COMMENT 'Таймстэмп дедлайна',
+    INDEX (user_id, project_id)
 );
 
-CREATE INDEX index_user ON users(id, dt_registered, email);
-
-CREATE INDEX index_project ON projects(id, project_name);
-
-CREATE INDEX index_tasks ON projects(id, task_name, dt_add, dt_deadline);
