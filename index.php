c8aba24108aa0cd9b3f5a125ea5bb696547f1dcd
Дмитрий <galigin01@gmail.com>
2022-05-19 19:45:10 +0300

    Удалил пустую папку doengsdone

--- index.php
+++ index.php
@@ -4,6 +4,15 @@ require_once 'helpers.php';
 // показывать или нет выполненные задачи
 $show_complete_tasks = rand(0, 1);
 
+
+
+$user = [
+    'id' => 1,
+    'user_name' => 'Вася',
+    ];
+
+    
+
 $projects = [
     'Входящие',
     'Учеба',
