cbc0047a1aa8584270a77001d9851c3e0fa96887
Дмитрий <galigin01@gmail.com>
2022-06-03 12:35:25 +0300

    1-вариант, сделал самостоятельно, ждем замечаний

--- index.php
+++ index.php
@@ -22,13 +22,48 @@ $projects_result = mysqli_query($con, $projects)
 $projects_arr = mysqli_fetch_all($projects_result, MYSQLI_ASSOC);
  
 
-// получаем массив задач
-$tasks = "SELECT * FROM task";
-$tasks_result = mysqli_query($con, $tasks)
-    or exit('Ошибка получения массива задач');    
-$tasks_arr = mysqli_fetch_all($tasks_result, MYSQLI_ASSOC);
-    
 
+
+
+$params = $_GET;
+$scriptname = pathinfo(__FILE__, PATHINFO_BASENAME);
+
+
+// Получаем массив задач, но сначала
+// проверяем наличие get-параметра, если есть, 
+// то делаем запрос sql c условием, где project_id = get-параметру,
+// если нет, то берем все строки
+if (isset($params['project'])){
+    $get_param_project_id = $params['project'];
+    $tasks = "SELECT * FROM task WHERE project_id = $get_param_project_id";
+} else {
+    $tasks = "SELECT * FROM task";
+};
+$tasks_result = mysqli_query($con, $tasks)
+    or exit('Ошибка получения массива задач'); 
+$tasks_arr = mysqli_fetch_all($tasks_result, MYSQLI_ASSOC);
+
+
+
+
+// Проверяем наличие совпадений в get-параметре и массиве задач
+$checker_get_params = 0;
+$error = "Ошибка 404, такая страница отсутвует";
+foreach ($tasks_arr as $arr => $elem) {
+    if ($elem['project_id'] == $get_param_project_id) {
+        $checker_get_params++;
+    }   
+};
+
+
+if ($checker_get_params == 0 && $get_param_project_id) {
+    $layout = include_template(
+        'error.php',
+        [
+            'error' => $error,
+        ]
+    );
+} else {
 
 $main = include_template(
     'main.php',
@@ -48,9 +83,12 @@ $layout = include_template(
         'projects' => $projects_arr, 
         'tasks' => $tasks_arr, 
         'con' => $con,
+        'scr_name' =>$scriptname,
+        'params' => $params,
 
 
     ]
 );
+}
 
 print($layout);
